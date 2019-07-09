f3ab6224a843079ab7960b3b34705217902c7836
one <plin3000@gmail.com>
2019-07-09 11:48:53 -0400

    isPrime()

--- guesswhat/PrimeNumber.cpp
+++ guesswhat/PrimeNumber.cpp
@@ -3,6 +3,16 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+bool isPrime(int x) {
+
+	for (int i = 2; i <= x / i; i++) {
+		int factor = x / i;
+		if (factor * i == x)
+			return false;
+	}
+	return true;
+};
+
 int primenumber_main()
 {
 	int x;
@@ -12,17 +22,18 @@ int primenumber_main()
 	bool prime = true;
 	int i = 2;
 
-	while (i <= x / i)
-	{
-		int factor = x / i;
-		if (factor*i == x)
-		{
-			cout << "factor found: " << i << " * " << factor << endl;
-			prime = false;
-			break;
-		}
-		i = i + 1;
-	}
+	prime = isPrime(x);
+	//while (i <= x / i)
+	//{
+	//	int factor = x / i;
+	//	if (factor*i == x)
+	//	{
+	//		cout << "factor found: " << i << " * " << factor << endl;
+	//		prime = false;
+	//		break;
+	//	}
+	//	i = i + 1;
+	//}
 
 	cout << x << " is ";
 	if (prime)
